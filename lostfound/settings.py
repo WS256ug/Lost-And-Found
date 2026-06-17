3bfff6d586203249a4ee2c1990926a4854f5ac2d
WS256ug <wasswaswalleh@gmail.com>
2026-06-17 20:09:47 +0300

    `Added 'https://lost-and-found-ws-teams.vercel.app' to CORS_ALLOWED_ORIGINS list`

--- lostfound/settings.py
+++ lostfound/settings.py
@@ -134,5 +134,6 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:8100',
     'http://127.0.0.1:8100',
-    'lost-and-found-ws-teams.vercel.app'
+    'https://lost-and-found-ws-teams.vercel.app',
 ]
+
